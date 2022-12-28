138b38056bd26f852bd6501339b011a1368e9c92
Dayoung Jeong <109073112+dyjeong576@users.noreply.github.com>
2022-12-28 21:31:42 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);	cout.tie(NULL); 
+    
+    int num, x, y;
+    
+    cin >> num;
+    
+    while(num--){
+    	cin >> x >> y;
+    	cout << x + y << endl;
+	}
+    
+	
+	return 0;
+    
+}
