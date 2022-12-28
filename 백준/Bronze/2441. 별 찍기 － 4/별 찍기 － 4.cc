a25e6ded8822af995a0a60e4f2a8041296eababd
Dayoung Jeong <109073112+dyjeong576@users.noreply.github.com>
2022-12-29 00:45:49 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);	cout.tie(NULL); 
+    
+    int n, x, tmp;
+    
+    cin >> n;
+    x = n;
+    
+    for(int i=n; i>0; i--){
+    	tmp = n-i;
+    	while(tmp--){
+    		cout << " ";
+		}
+		tmp = i; 
+    	while(tmp--){
+    		cout << "*";
+		}
+    	cout << "\n";
+	}
+    
+	return 0;
+    
+}
