04880608a5d6e97d2d06b37a2ada352df5569994
Dayoung Jeong <109073112+dyjeong576@users.noreply.github.com>
2023-01-03 19:42:05 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ int dp[100001], tp, n;
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+		
+	cin >> n;
+	for(int i=1; i<=n; i++){
+		dp[i] = i;
+		for(int j = 1; j*j<=i; j++){
+			dp[i] = min(dp[i], dp[i-j*j] + 1);
+		}
+	}
+	
+	cout << dp[n] << endl;
+
+}
