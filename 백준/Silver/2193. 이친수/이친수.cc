88601ecca736fca18c41a0a82a8f5622347d5646
Dayoung Jeong <109073112+dyjeong576@users.noreply.github.com>
2023-01-03 16:52:58 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[94];
+int n, t;
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	dp[1] = 1; dp[2] = 1;
+	cin>> n;
+	for(int i=3; i<=n; i++){
+		dp[i] = dp[i-1] + dp[i-2];
+	}
+	
+	cout << dp[n] << endl;
+	
+	return 0;
+	
+}
